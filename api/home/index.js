f9cf196e16ef617982d41a0b2cdd8811ca92ed38
EGOIST <0x142857@gmail.com>
2019-10-08 21:38:18 +0800

    load README.md from denopkg.com

--- api/home/index.js
+++ api/home/index.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   text =
     text ||
     (await fetch(
-      `https://cdn.jsdelivr.net/gh/denopkg/denopkg.com/README.md`
+      `http://denopkg.com/denopkg/denopkg.com/README.md`
     ).then(res => res.text()))
   res.setHeader('content-type', 'text/html')
   res.end(`
