06ea971cdde690a76f7a9e106d85130e667e9a24
EGOIST <0x142857@gmail.com>
2020-05-17 19:49:01 +0800

    use next.js

---
+++ src/pages/api/github/[...slug].ts
@@ -0,0 +1,31 @@
+import { NextApiHandler, NextApiResponse } from 'next'
+
+const MATCHER = /^([^\/]+)\/([^\/@]+)(@)?(.*)/
+
+const invalidURL = (res: NextApiResponse) => {
+  res.status(500)
+  res.end(`Invalid URL`)
+}
+
+const handler: NextApiHandler = async (req, res) => {
+  const slug = Array.isArray(req.query.slug) ? req.query.slug : [req.query.slug]
+  const m = MATCHER.exec(slug.join('/'))
+
+  if (!m) {
+    return invalidURL(res)
+  }
+  const [, owner, repo, versionSpecified, rest] = m
+  if (!owner || !repo) {
+    return invalidURL(res)
+  }
+  res.status(301)
+  res.setHeader(
+    'Location',
+    `https://raw.githubusercontent.com/${owner}/${repo}/${
+      versionSpecified ? '' : 'master'
+    }${rest || '/mod.ts'}`
+  )
+  res.end()
+}
+
+export default handler
