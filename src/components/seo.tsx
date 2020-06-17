b9053fc481a77f87e38ce9aee8ec586b79dfb8fe
Italo <italusousa@gmail.com>
2020-06-16 23:06:15 -0300

    add favicon

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -12,6 +12,10 @@ export default () => {
   return (
     <Head>
       <title>{values.title}</title>
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <meta name="title" content={values.title} key="title" />
       <meta name="description" content={values.description} key="description" />
       <meta property="og:url" content="https://denopkg.com/" key="og-url" />
