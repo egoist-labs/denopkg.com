b27d5ed93fb8d1c3f7b4950e3b805ad502f5cb1d
Italo <italusousa@gmail.com>
2020-06-16 22:44:46 -0300

    fix: twitter meta property

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -12,14 +12,16 @@ export default () => {
   return (
     <Head>
       <title>{values.title}</title>
-      <meta property="description" content={values.description} key="description" />
+      <meta name="title" content={values.title} key="title" />
+      <meta name="description" content={values.description} key="description" />
+      <meta property="og:url" content="https://denopkg.com/" key="og-url" />
       <meta property="og:title" content={values.title} key="og-title" />
       <meta property="og:description" content={values.description} key="og-description" />
       <meta property="og:type" content="website" key="og-type" />
-      <meta property="twitter:card" content="summary" key="twitter-card" />
-      <meta property="twitter:creator" content={values.creator} key="twitter-creator" />
-      <meta property="twitter:title" content={values.title} key="twitter-title" />
-      <meta property="twitter:description" content={values.description} key="twitter-description" />
+      <meta name="twitter:title" content={values.title} key="twitter-title" />
+      <meta name="twitter:card" content="summary" key="twitter-card" />
+      <meta name="twitter:creator" content={values.creator} key="twitter-creator" />
+      <meta name="twitter:description" content={values.description} key="twitter-description" />
     </Head>
   );
 }
